f1e632c412b21d2154b08e8180277c80a861e9b5
Pratik Parmar <prateek18597@gmail.com>
2018-12-19 12:22:01 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,69 @@
+var html_content,css_content,js_content;
+var count_control=0;
+
+
+window.addEventListener("load", function() {
+  	
+  	document.getElementById("file-upload-html").onchange = function(event)
+  	{
+	    var reader = new FileReader();
+	    reader.readAsText(event.srcElement.files[0]);
+	    var me = this;
+	    console.log("Event is working");
+	    reader.onload = function () {
+	      var fileContent = reader.result;
+	      html_content=fileContent;
+		  console.log(html_content);
+    	}
+	}
+	
+	document.getElementById("file-upload-css").onchange = function(event) {
+	    var reader = new FileReader();
+	    reader.readAsText(event.srcElement.files[0]);
+	    var me = this;
+	    console.log("Event is working");
+	    reader.onload = function () {
+	      var fileContent = reader.result;
+	      css_content=fileContent;
+		  console.log(css_content);
+	    }
+	}
+	
+	document.getElementById("file-upload-js").onchange = function(event) {
+	    var reader = new FileReader();
+	    reader.readAsText(event.srcElement.files[0]);
+	    var me = this;
+	    console.log("Event is working");
+	    reader.onload = function () {
+	      var fileContent = reader.result;
+	      js_content=fileContent;
+		  console.log(js_content);
+    	}
+	}
+});
+
+function addControl()
+{
+	count_control+=1;
+	var g = document.createElement('div');
+	g.setAttribute("id","field"+count_control);
+	g.setAttribute("class","inputField");
+	var phone = document.createElement("INPUT");
+	phone.setAttribute("type", "text");
+	phone.setAttribute("value", "Phone Key");
+	var desktop = document.createElement("INPUT");
+	desktop.setAttribute("type", "text");
+	desktop.setAttribute("value", "Desktop Key");
+	phone.setAttribute("id","phone"+count_control);
+	desktop.setAttribute("id","desktop"+count_control);
+	phone.onclick=function(){
+		phone.value="";
+	}
+	desktop.onclick=function(){
+		desktop.value="";
+	}
+	g.appendChild(phone);
+	g.appendChild(desktop);
+	var parent=document.getElementById("inputControl");
+	parent.appendChild(g);
+}
